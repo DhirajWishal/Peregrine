cf1f59322428106c7622d99ff0d5ae838eb0b8e6
Dhiraj Wishal <wishaldhiraj@gmail.com>
2023-05-17 12:18:33 +0530

    Added some improvements to the receiver class.
    * Added more debugging for the nRF24L01 data link.

--- src/components/nRF24L01DataLink.cpp
+++ src/components/nRF24L01DataLink.cpp
@@ -1,7 +1,13 @@
 // Copyright 2023 Nexonous
 // SPDX-License-Identifier: Apache-2.0
 
+#include <SPI.h>
 #include "nRF24L01DataLink.hpp"
+
+#ifdef PEREGRINE_DEBUG
+#include "printf.h"
+
+#endif
 
 #include "core/Logging.hpp"
 
@@ -14,9 +20,27 @@ void nRF24L01DataLink::onInitialize()
 {
 	PEREGRINE_PRINTLN("Initializing the nRF24L01 transceiver.");
 
-	m_Transceiver.begin();
+	if (!m_Transceiver.begin())
+	{
+		PEREGRINE_PRINTLN("Failed to initialize the nRF24L01 transceiver!");
+		return;
+	}
+
+	m_Transceiver.setDataRate(RF24_250KBPS);
+
 	m_Transceiver.openReadingPipe(0, g_DefaultAddress);
 	m_Transceiver.startListening();
+
+#ifdef PEREGRINE_DEBUG
+	m_Transceiver.setAutoAck(1);
+	m_Transceiver.enableAckPayload();
+	m_Transceiver.setRetries(5, 15);
+	m_Transceiver.setPALevel(RF24_PA_MIN);
+
+	printf_begin();
+	m_Transceiver.printPrettyDetails();
+
+#endif
 
 	PEREGRINE_PRINTLN("The nRF24L01 transceiver is initialized.");
 }
